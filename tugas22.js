9c4dbcfe3f5beb2ec6e0dd1df1fe0f409e6d6484
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-06-30 21:46:53 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function ulang() {
+    let a = ["Saya", "ingin", "belajar", "bersama"];
+
+    a.forEach(function (item, index) {
+        console.log("Item : " + item + " Index ke " + index);
+    });
+}
+
+ulang();
